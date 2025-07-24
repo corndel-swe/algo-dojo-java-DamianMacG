4df083dce3c8b27bd7cd7b3f85f544ce74ee6603
damiang <damiangildea12@gmail.com>
2025-07-24 16:15:13 +0100

    Completed two pointer tasks

--- src/main/java/com/corndel/two_pointers/array_filter/ArrayFilter.java
+++ src/main/java/com/corndel/two_pointers/array_filter/ArrayFilter.java
@@ -1,12 +1,48 @@
 package com.corndel.two_pointers.array_filter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ArrayFilter {
 
     public static int[] getDistinctNumbersBruteForce(int[] arr) {
-        return new int[0];
+        List<Integer> list = new ArrayList<>();
+
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+
+
+        for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] != arr[i + 1]) {
+                    list.add(arr[i]);
+                }
+
+        }
+        list.add(arr[arr.length - 1]);
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
     public static int[] getDistinctNumbersTwoPointers(int[] arr) {
-        return new int[0];
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+
+        int[] copiedArray = arr.clone();
+
+        int write = 1;
+
+        for (int i = 1; i < copiedArray.length; i++) {
+
+            if (copiedArray[i] != copiedArray[i - 1]) {
+                copiedArray[write] = copiedArray[i];
+                write++;
+            }
+        }
+
+        return Arrays.copyOfRange(copiedArray, 0, write);
+
     }
 }
--- src/main/java/com/corndel/two_pointers/array_reverser/ArrayReverser.java
+++ src/main/java/com/corndel/two_pointers/array_reverser/ArrayReverser.java
@@ -1,13 +1,37 @@
 package com.corndel.two_pointers.array_reverser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayReverser {
     public static int[] reverseBruteForce(int[] arr) {
-        return new int[0];
+        List<Integer> list = new ArrayList<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            list.add(arr[i]);
+        }
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
 
     public static int[] reverseTwoPointers(int[] arr) {
-        return new int[0];
+        int[] copiedArray = arr.clone();
+
+        int left = 0;
+        int right = copiedArray.length - 1;
+
+        while (left < right) {
+            int temp = copiedArray[left];
+            copiedArray[left] = copiedArray[right];
+            copiedArray[right] = temp;
+
+            left++;
+            right--;
+        }
+
+
+        return Arrays.copyOfRange(copiedArray, 0, left);
+
     }
 
 }
